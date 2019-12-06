86d9d3a63e82ab4336d50d76188d6ba857564967
LiamMcIvor <liam.mcivor1@gmail.com>
2019-12-06 09:12:24 +0000

    Setup commit

---
+++ src/test/java/javaAssessmentTest/JavaAssessmentTest.java
@@ -0,0 +1,18 @@
+package javaAssessmentTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class JavaAssessmentTest {
+	private boolean test = false;
+
+	@Test
+	public void test() {
+		
+		assertEquals(true, test );
+	}
+
+}
