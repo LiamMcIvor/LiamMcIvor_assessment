a9e236209413b4dc6d04c087e26af28be276e8df
LiamMcIvor <liam.mcivor1@gmail.com>
2019-12-06 12:45:20 +0000

    added location tests

--- src/test/java/javaAssessmentTest/JavaAssessmentTest.java
+++ src/test/java/javaAssessmentTest/JavaAssessmentTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import junit.framework.Assert;
 import main.Compass;
 import main.Item;
+import main.Location;
 import main.Player;
 
 public class JavaAssessmentTest {
@@ -18,10 +19,23 @@ public class JavaAssessmentTest {
 		Player player = new Player();
 		Compass compass = new Compass();
 		Item item = new Item();
+		Location location = new Location();
 		
-//		player.setInitialLocation();
+		player.setInitialLocation();
 		assertNotSame(0, player.xLocation);
+		assertNotSame(0, player.yLocation);
 		
+		item.setInitialLocation();
+		assertNotSame(0, item.xLocation);
+		assertNotSame(0, item.yLocation);
+		
+		location.setInitialLocation();
+		assertNotSame(0, location.xLocation);
+		assertNotSame(0, location.xLocation);
+		
+		player.move("N");
+		
+//		assertNotSame(player.xLocation, player.move("N"));
 		
 		
 //		assertEquals(true, test );
